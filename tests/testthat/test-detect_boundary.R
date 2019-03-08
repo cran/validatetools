9f2fe46d35e2f2838c4cb889501f388426be5819
Edwin de Jonge <edwindjonge@gmail.com>
2019-03-08 10:50:03 +0000

    version 0.4.6

--- tests/testthat/test-detect_boundary.R
+++ tests/testthat/test-detect_boundary.R
@@ -21,12 +21,24 @@ test_that("detect num boundary works", {
   expect_equal(as.list(bounds[1,]), list(variable="x", lowerbound=1, upperbound=Inf))
 })
 
+test_that("detect num boundary works without num variables", {
+  rules <- validate::validator(
+    if (A == "a")  B == "b"
+  )
+  bounds <- detect_boundary_num(rules)
+  expect_equal(nrow(bounds), 0)
+})
+
 test_that("detect cat boundary works", {
   rules <- x <-  validator( x > 1
                           , if (x > 0) A == 'a1'
                           , B %in% c("b1", "b2")
                           )
   cat_bounds <- detect_boundary_cat(rules)
+  expect_equal(cat_bounds$A, "a1")
+  expect_equal(cat_bounds$B, c("b1", "b2"))
+  
+  cat_bounds <- detect_boundary_cat(rules, as_df = TRUE)
   expect_equal(as.list(cat_bounds[1,]), list(variable="A", value="a1", min=1, max=1))
   expect_equal(as.list(cat_bounds[2,]), list(variable="B", value="b1", min=0, max=1))
   expect_equal(as.list(cat_bounds[3,]), list(variable="B", value="b2", min=0, max=1))
@@ -40,6 +52,9 @@ test_that("detect cat boundary works for longer named variables", {
   )
   
   cat_bounds <- detect_boundary_cat(rules)
+  expect_equal(cat_bounds, list(job="yes"))
+  
+  cat_bounds <- detect_boundary_cat(rules, as_df = TRUE)
   expect_equal(as.list(cat_bounds[1,]), list(variable="job", value="yes", min=1, max=1))
   expect_equal(as.list(cat_bounds[2,]), list(variable="job", value="no", min=0, max=0))
 })
